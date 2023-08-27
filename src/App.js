24abb3a531d0e9bcf08822ff5f527fc0d9fea4b6
Irfan Ullah <irfan.ullah@arcelik.com>
2023-08-27 13:07:14 +0500

    adding components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Home from './pages/home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Products from './pages/products/Products';
+import Users from './pages/users/Users';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='products' element={<Products />} />
+          <Route path='users' element={<Users />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './menu.scss'
+const Menu = () => {
+  return (
+    <div className='menu'>
+      
+    </div>
+  )
+}
+
+export default Menu
